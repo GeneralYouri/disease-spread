f4a917159b795d464d0d55f09980b52f748ab11d
Youri <y.bok@students.uu.nl>
2023-06-15 21:09:08 +0200

    Refactor neighborhood

--- neighborhood.py
+++ neighborhood.py
@@ -8,22 +8,32 @@ class Strategy(Enum):
 
 # Moore; 8-neighborhood
 def Moore(r):
-    coordinates = []
-    for y in range(-r, r + 1):
-        for x in range(-r, r + 1):
-            if x == 0 and y == 0:
-                continue;
-            coordinates.append((x, y))
-    return coordinates
+    axis = range(-r, r + 1)
+    return [(x, y) for x in axis for y in axis if 0 < abs(x) + abs(y)]
 
 # Neumann; 4-neighborhood
 def Neumann(r):
-    coordinates = []
-    for y in range(-r, r + 1):
-        for x in range(-r, r + 1):
-            if x == 0 and y == 0:
-                continue;
-            if abs(x) + abs(y) > r:
-                continue;
-            coordinates.append((x, y))
-    return coordinates
+    axis = range(-r, r + 1)
+    return [(x, y) for x in axis for y in axis if 0 < abs(x) + abs(y) <= r]
+
+# # Moore; 8-neighborhood
+# def Moore(r):
+#     coordinates = []
+#     for y in range(-r, r + 1):
+#         for x in range(-r, r + 1):
+#             if x == 0 and y == 0:
+#                 continue;
+#             coordinates.append((x, y))
+#     return coordinates
+
+# # Neumann; 4-neighborhood
+# def Neumann(r):
+#     coordinates = []
+#     for y in range(-r, r + 1):
+#         for x in range(-r, r + 1):
+#             if x == 0 and y == 0:
+#                 continue;
+#             if abs(x) + abs(y) > r:
+#                 continue;
+#             coordinates.append((x, y))
+#     return coordinates
